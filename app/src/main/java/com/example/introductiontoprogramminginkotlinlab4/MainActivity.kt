8de68ad3bf8955fe7f31c2a6457bde27e75e8def
Anatoly <vergolant@gmail.con>
2024-09-19 16:06:13 +0300

    First application complete

--- app/src/main/java/com/example/introductiontoprogramminginkotlinlab4/MainActivity.kt
+++ app/src/main/java/com/example/introductiontoprogramminginkotlinlab4/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.introductiontoprogramminginkotlinlab4.ui.theme.IntroductionToProgrammingInKotlinLab4Theme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntroductionToProgrammingInKotlinLab4Theme {
-        Greeting("Android")
+        Greeting("Anatoly")
     }
 }
